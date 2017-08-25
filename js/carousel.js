01a4b38305128877fd4b6f315a975e58b0952db1
theMADAIguy <mad.ai.guy@gmail.com>
2017-08-25 19:39:20 +0530

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+//
+//  Portfolio
+//  carousel.js
+//
+//  Created By Ryan Maugin on 06/05/17
+//
+
+$(document).ready(function () {
+    
+
+    $('.experiences-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 12000,
+        dots: true,
+    });
+
+});
